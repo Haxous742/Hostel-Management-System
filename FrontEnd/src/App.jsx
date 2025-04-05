7ec1461fda994298d39a58dfbe3efd1f66e299ff
Haxous742 <vedantmundada6@gmail.com>
2025-04-05 13:05:07 +0530

    Logo

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,7 +4,9 @@ import { Route,Routes } from 'react-router-dom'
 import Signup from './signup'
 import Dashboard from './dashboard'
 import LandingPage from './LandingPage'
+import Menu from './Menu'
 import './App.css'
+
 
 
 const App = () => {
@@ -15,7 +17,12 @@ const App = () => {
         <Route index element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-
+        <Route path="/dashboard/Menu" element={<Menu />} />
+        <Route path="/dashboard/Community" element={<Dashboard />} />
+        <Route path="/dashboard/Complaints" element={<Dashboard />} />
+        <Route path="/dashboard/Profile" element={<Dashboard />} />
+        <Route path="/dashboard/Leave" element={<Dashboard />} />
+        <Route path="/dashboard/Leave/Cab-Sharing" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your project structure
+      "./src/**/*.{js,ts,jsx,tsx}", 
+      "./node_modules/flowbite/**/*.js",
+      "./node_modules/flowbite-react/**/*.js",
     ],
     theme: {
       extend: {},
