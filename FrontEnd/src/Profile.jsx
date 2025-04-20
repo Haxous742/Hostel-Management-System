6f2dc413a22366f30fd3ccb31be35c6bd40ff3d0
Aryan Sharma <aryansharma0305@gmail.com>
2025-04-20 12:10:20 +0530

    Update Profile.jsx

--- FrontEnd/src/Profile.jsx
+++ FrontEnd/src/Profile.jsx
@@ -173,9 +173,9 @@ const Profile = () => {
           </button>
 
           {isProfilePopupOpen && (
-            <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
-              <div className="bg-slate-800 rounded-lg p-6 w-96 shadow-lg relative">
-                <div className="flex justify-between items-center mb-4">
+           <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+           <div className="bg-slate-800/80 backdrop-blur-md rounded-lg p-6 w-96 shadow-lg relative">
+             <div className="flex justify-between items-center mb-4">
                   <h2 className="text-lg font-semibold">
                     Update Profile Picture
                   </h2>
