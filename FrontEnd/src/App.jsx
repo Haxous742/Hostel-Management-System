0f3b8aa7c7ac51f24b7d8d8d9e65431529fa15e5
Haxous742 <vedantmundada6@gmail.com>
2025-04-20 20:53:25 +0530

    admin

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,6 +13,15 @@ import Profile from './Profile';
 import ProtectedRoute from './ProtectedRoute'; 
 import YourPosts from './yourposts';
 
+import A_Dashboard from './Admin/dashboard';
+import A_Complaints from './Admin/complaints';
+import A_Mess from './Admin/mess';
+import A_Community from './Admin/community';
+import A_Leave from './Admin/leave';
+import A_Profile from './Admin/profile';
+import A_YourPosts from './Admin/yourposts';
+import Admin from './Admin/Admin';
+
 import PublicProfile from './PublicProfile';
 
 const App = () => {
@@ -31,8 +40,17 @@ const App = () => {
         <Route path="/dashboard/Complaints" element={<ProtectedRoute><Complaints /></ProtectedRoute>} />
         <Route path="/dashboard/Profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="/dashboard/Leave" element={<ProtectedRoute><Leave /></ProtectedRoute>} />
-        <Route path="/dashboard/Leave/Cab-Sharing" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
 
+        <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+        <Route path="/admin/dashboard" element={<ProtectedRoute><A_Dashboard /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/mess" element={<ProtectedRoute><A_Mess /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/Community" element={<ProtectedRoute><A_Community /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/Community/yourposts" element={<ProtectedRoute><A_YourPosts /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/Complaints" element={<ProtectedRoute><A_Complaints /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/Profile" element={<ProtectedRoute><A_Profile /></ProtectedRoute>} />
+        <Route path="/admin/dashboard/Leave" element={<ProtectedRoute><A_Leave /></ProtectedRoute>} />
+
+        {/* Public routes */}
 
         <Route path="/user/:userId" element={<ProtectedRoute><PublicProfile /></ProtectedRoute>} />
 
